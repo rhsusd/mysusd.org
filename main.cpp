22fc535464b85faf9b119543ffd7c13d14148eb7
EC2 Default User <ec2-user@ip-172-31-27-153.us-east-2.compute.internal>
2024-05-21 16:19:50 +0000

    testing

--- main.cpp
+++ main.cpp
@@ -3,17 +3,14 @@
 #include <iostream>
 
 int main() {
-    uWS::SSLApp({
-        .cert_file_name = "cert.pem",
-        .key_file_name = "key.pem"
-    }).get("/p", [](auto *res, auto *req) {
-        
-    }).listen(443, [](auto *listenSocket) {
+    uWS::App().get("/p", [](auto *res, auto *req) {
+        res->end();
+    }).listen(80, [](auto *listenSocket) {
         if (listenSocket) {
             std::cout << "Listening on port " << 443 << std::endl;
         } else {
             std::cout << "Failed to load certs or to bind to port" << std::endl;
         }
-    })
+    });
     return 0;
 }
